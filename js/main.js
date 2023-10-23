99bd9f7b6185ef88376cc2a7d54e091d9114c8cc
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-23 15:16:57 +0200

    add somma

--- js/main.js
+++ js/main.js
@@ -6,7 +6,20 @@ createApp({
     data() {
       return {
         message: 'Hello Vue!',
-        imgSrc: 'https://picsum.photos/200/300'
+        imgSrc: 'https://picsum.photos/200/300',
+        bgGreen: 'bg-green',
+        inputValue:'cosa vedi nell \' immagine ',
+        numA: 2,
+        numB: 6,
       }
+    },
+    methods: {
+        onClick () {
+            console.log('hai vinto un milione di euro');
+        },
+        somma (a, b) {
+            return a + b
+        },
+        
     }
   }).mount('#app')
